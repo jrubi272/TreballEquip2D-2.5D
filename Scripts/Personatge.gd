1ac3a4390beef3fecbdc6f49f488ce5d2039ae79
jrubi272 <jaumerubi7000@gmail.com>
2023-06-12 22:11:11 +0200

    Personaje + texturas y tal

---
+++ Scripts/Personatge.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+class_name Personatge
+
+#constants
+const FRICTION: float = 0.15
+
+#export variables
+export(int) var accel:= 40
+export(int) var max_vel:= 100
+
+#Variables/Atributs
+var direccio: Vector2 = Vector2.ZERO
+var velocitat: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocitat = move_and_slide(velocitat)
+	velocitat = lerp(velocitat, Vector2.ZERO, FRICTION)#per a disminuir gradualment la velocitat del personatge
+
+func moure():
+	direccio = direccio.normalized()
+	velocitat += velocitat * accel
+	velocitat = velocitat.clamped(max_vel) #per a limitar la velocitat del personatge
