5968962bb3c11cf96cfbe85806c14516a7f09ebc
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-06-16 20:32:39 +0200

    kj

--- Scripts/Estats.gd
+++ Scripts/Estats.gd
@@ -2,39 +2,39 @@ class_name Estats extends Node
 
 var estats: Dictionary = {}
 var estat_anterior: int = -1
-var estat: int = -1 setget set_state
+var estat: int = -1 setget colocar_estat
 
 onready var pare: Personatge = get_parent()
 onready var anim_player: AnimationPlayer = pare.get_node("AnimationPlayer")
 
 func _physics_process(delta: float) -> void:
 	if estat != -1:
-		_state_logic(delta)
-		var transicio: int = _get_transition()
+		comportament_estats(delta)
+		var transicio: int = transicionar()
 		if transicio != -1:
-			set_state(transicio)
+			colocar_estat(transicio)
 
 
-func _state_logic(_delta: float) -> void:
+func comportament_estats(_delta: float) -> void:
 	pass
 	
 	
-func _get_transition() -> int:
+func transicionar() -> int:
 	return -1
 	
-func _add_state(new_state: String) -> void:
+func afegir_estat(new_state: String) -> void:
 	estats[new_state] = estats.size()
 	
 	
-func set_state(new_state: int) -> void:
-	_exit_state(estat)
+func colocar_estat(new_state: int) -> void:
+	sortir_estat(estat)
 	estat_anterior = estat
 	estat = new_state
-	_enter_state(estat_anterior, estat)
+	entrar_estat(estat_anterior, estat)
 	
-func _enter_state(estat_anterior: int, _new_state: int) -> void:
+func entrar_estat(estat_anterior: int, _new_state: int) -> void:
 	pass
 	
 	
-func _exit_state(_state_exited: int) -> void:
+func sortir_estat(_state_exited: int) -> void:
 	pass
