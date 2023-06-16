17602dfa8c44d814505ab9211b508bec19e4ef91
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-06-16 22:37:07 +0200

    f

--- Scripts/Enemic.gd
+++ Scripts/Enemic.gd
@@ -1,13 +1,15 @@
 class_name Enemic extends Personatge
 
-onready var jugador: KinematicBody2D = get_tree().current_scene.get_node("P_Jugador")
-onready var localitzacio: Navigation2D = get_tree().current_scene.get_node("Navigation2D")
-
+var jugador: KinematicBody2D
+var timer: Timer
+var localitzacio: Navigation2D
 var cami: PoolVector2Array
 
 
 func _ready():
-	connect("tree_exited", get_parent(), "_on_enemy_killed")
+	jugador = get_tree().current_scene.get_node("P_Jugador")
+	timer = get_node("Timer")
+	localitzacio = get_tree().current_scene.get_node("Navigation2D")
 
 func perseguir_jugador() -> void:
 	if cami:
@@ -17,21 +19,21 @@ func perseguir_jugador() -> void:
 			cami.remove(0)
 			if not cami:
 				return
-		direccio = seguentLloc
-		if seguentLloc.x > 0 and anim_sprite.flip_h:
-			anim_sprite.flip_h = false
-		elif seguentLloc.x < 0 and not anim_sprite.flip_h:
-			anim_sprite.flip_h = true
-
-
-func obtenirCamiJugador() -> void:
-	cami = localitzacio.get_simple_path(global_position, jugador.position)
+			direccio = seguentLloc
+			if seguentLloc.x > 0 and anim_sprite.flip_h:
+				anim_sprite.flip_h = false
+			elif seguentLloc.x < 0 and not anim_sprite.flip_h:
+				anim_sprite.flip_h = true
 
 
 func _on_Timer_timeout():
 	if is_instance_valid(jugador):
 		obtenirCamiJugador()
 	else:
-		$Timer.stop()
+		timer.stop()
 		cami = []
 		direccio = Vector2.ZERO
+
+
+func obtenirCamiJugador() -> void:
+	cami = localitzacio.get_simple_path(global_position, jugador.position)
