d00869ae5fb8e3fedaa9f8d2c3cf083b68963dc2
jrubi272 <jaumerubi7000@gmail.com>
2023-06-16 23:55:59 +0200

    ogre movimiento

--- Scripts/EstatsOgre.gd
+++ Scripts/EstatsOgre.gd
@@ -11,9 +11,9 @@ func comportament_estats(_delta: float) -> void:
 	if estat == estats.caminar:
 		pare.perseguir_jugador()
 		pare.moure()
-	
+
 func entrar_estat(estat_anterior: int, nou: int) -> void:
 	match nou:
 		estats.caminar:
 			anim_player.play("Caminar")
-	
+
