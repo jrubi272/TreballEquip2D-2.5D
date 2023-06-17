593c3cba0ff9b54b844981003111c3d93d9f558e
jrubi272 <jaumerubi7000@gmail.com>
2023-06-17 02:58:57 +0200

    Escenarios creados

---
+++ Scenes/Escenari.gd
@@ -0,0 +1,16 @@
+class_name Escenari extends Node2D
+
+onready var tileHerba = get_node("Herba")
+onready var tileParts = get_node("PartsDarreraJugador")
+onready var tileObj = get_node("Objectes")
+onready var tileEscales = get_node("Escales")
+onready var tileParets = get_node("Parets")
+onready var tilePlantes = get_node("Plantes")
+
+func canviar_collisions(capa:int, valor:bool):
+	tileHerba.set_collision_layer_bit(capa, valor)
+	tileParts.set_collision_layer_bit(capa, valor)
+	tileObj.set_collision_layer_bit(capa, valor)
+	tileEscales.set_collision_layer_bit(capa, valor)
+	tileParets.set_collision_layer_bit(capa, valor)
+	tileEscales.set_collision_layer_bit(capa, valor)
