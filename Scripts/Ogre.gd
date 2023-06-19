91ae2ce8c17406402ac44749364b657dfd6ccaa0
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-06-19 20:01:50 +0200

    dsd

--- Scripts/Ogre.gd
+++ Scripts/Ogre.gd
@@ -1,7 +1,6 @@
 class_name Ogre extends Enemic
 
-onready var hitbox: Area2D = get_node("Hitbox")
 
 
 func _process(delta):
-	hitbox.direccio_empenta = velocitat.normalized()
+	$Hitbox.direccio_empenta = velocitat.normalized()
