0da199bd538441b27b7e617ee7535f1fc47478d8
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-06-19 20:31:56 +0200

    fd

--- Scripts/Projectil.gd
+++ Scripts/Projectil.gd
@@ -5,7 +5,6 @@ export var velocitat:= 400
 export var dany = 1
 
 var direccio:= Vector2()
-var sortit = false
 
 
 func ini(dir:Vector2, posInicial:Vector2):
@@ -18,16 +17,7 @@ func _physics_process(delta):
 	position += direccio * velocitat * delta
 
 
-func _on_Projectil_body_exited(body):
-	if not sortit:
-		sortit = true
-		set_collision_mask_bit(0, true)
-		set_collision_mask_bit(1, true)
-		set_collision_mask_bit(2, true)
-		set_collision_mask_bit(3, true)
-
 func colisionar(body: KinematicBody2D) -> void:
-	if sortit:
-		if body != null:
-			body.rebre_dany(danys, direccio_empenta, forca_empenta)
-		queue_free()
+	if body != null:
+		body.rebre_dany(danys, direccio_empenta, forca_empenta)
+	queue_free()
